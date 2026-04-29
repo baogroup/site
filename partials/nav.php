72d6a8a1c1e2d938df002c03b0bfb005d42787a8
baogroup <165123236+baogroup@users.noreply.github.com>
2026-04-29 18:02:59 +0300

    Add About section link

--- partials/nav.php
+++ partials/nav.php
@@ -1,6 +1,7 @@
-<nav class="nav">
+<nav class="main-nav">
   <ul class="nav-list">
     <li><a href="#services"><?= htmlspecialchars($t['nav_services']) ?></a></li>
+    <li><a href="#about"><?= htmlspecialchars($t['nav_about'] ?? 'About') ?></a></li>
     <li><a href="#process"><?= htmlspecialchars($t['nav_workflow']) ?></a></li>
     <li><a href="#prices"><?= htmlspecialchars($t['nav_packages']) ?></a></li>
     <li><a href="#contact"><?= htmlspecialchars($t['nav_contact']) ?></a></li>
