adbda8f88742a321dd5f17594b04b225f332b7d3
baogroup <165123236+baogroup@users.noreply.github.com>
2026-04-29 14:22:57 +0300

    Add PHP nav partial

---
+++ partials/nav.php
@@ -0,0 +1,8 @@
+<nav class="nav">
+  <ul class="nav-list">
+    <li><a href="#services"><?= htmlspecialchars($t['nav_services']) ?></a></li>
+    <li><a href="#process"><?= htmlspecialchars($t['nav_workflow']) ?></a></li>
+    <li><a href="#prices"><?= htmlspecialchars($t['nav_packages']) ?></a></li>
+    <li><a href="#contact"><?= htmlspecialchars($t['nav_contact']) ?></a></li>
+  </ul>
+</nav>
